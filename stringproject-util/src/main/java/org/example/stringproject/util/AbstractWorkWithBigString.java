36c848eef5d1736901adf590a07b40c8f14ccead
Aleksey Mikaelyan <mikaelaleksey@gmail.com>
2020-06-19 12:21:10 +0300

    example

---
+++ stringproject-util/src/main/java/org/example/stringproject/util/AbstractWorkWithBigString.java
@@ -0,0 +1,7 @@
+package org.example.stringproject.util;
+
+public interface AbstractWorkWithBigString {
+    String countCharsAndSortChars(String s);
+    String countWordsAndSortWords(String s);
+    String reverseWords(String s);
+}
